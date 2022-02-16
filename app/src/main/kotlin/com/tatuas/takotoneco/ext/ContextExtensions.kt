506f526f41117d13c196ee74587923ed76e3e0aa
Tatsuya Sawai <a209050@gmail.com>
2022-02-16 21:23:46 +0900

    Refine context ext class

--- app/src/main/kotlin/com/tatuas/takotoneco/ext/ContextExtensions.kt
+++ app/src/main/kotlin/com/tatuas/takotoneco/ext/ContextExtensions.kt
@@ -1,8 +1,10 @@
 package com.tatuas.takotoneco.ext
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.annotation.ColorRes
 
+@SuppressLint("PrivateResource")
 @ColorRes
 fun Context.getImagePlaceholderColor(): Int {
     val attrs = intArrayOf(com.google.android.material.R.attr.colorOnSurfaceVariant)
